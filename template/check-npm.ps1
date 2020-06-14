3d9f31f9de4225e1a0944210f7667d9f8c966674
Ali Khashan <ali.khashan@dekiru.se>
2020-06-14 15:53:16 +0200

    Check-npm with prettier output.

--- template/check-npm.ps1
+++ template/check-npm.ps1
@@ -1,15 +1,31 @@
- if ( -not (Test-Path -Path 'node_modules' -PathType Container) ) { 
-   "Node_modules folder is missing => Running ""npm install""" 
+Write-Host ""
+Write-Host "Checking if folder ""node_modules"" exists" -ForegroundColor Yellow
+if ( -not (Test-Path -Path 'node_modules' -PathType Container) ) { 
+   Write-Host "Node_modules folder is missing" -ForegroundColor Red -NoNewline 
+   Write-Host " => Running"  -ForegroundColor White -NoNewline 
+   Write-Host " ""npm install""" -ForegroundColor Green -NoNewline 
+   Write-Host " &" -ForegroundColor White -NoNewline 
+   Write-Host " ""npm install --save-dev check-dependencies""" -ForegroundColor Green -NoNewline 
+   Write-Host " &" -ForegroundColor White -NoNewline 
+   Write-Host " ""npm audit fix""" -ForegroundColor Green
    npm install
    npm install --save-dev check-dependencies
    npm audit fix
+   return
 }
-elseif ( -not (Test-Path -Path 'node_modules/check-dependencies' -PathType Container) ) {
-   "Package ""check-dependencies"" is not installed => Running ""npm install --save-dev check-dependencies""" 
-   npm install --save-dev check-dependencies 
-   npm audit fix
+Write-Host "Checking if npm package ""check-dependencies"" exists" -ForegroundColor Yellow
+if ( -not (Test-Path -Path 'node_modules/check-dependencies' -PathType Container) ) {
+   Write-Host "Package ""check-dependencies"" is not installed" -ForegroundColor Red -NoNewline 
+   Write-Host " => Running" -ForegroundColor White -NoNewline 
+   Write-Host " ""npm install --save-dev check-dependencies""" -ForegroundColor Green -NoNewline 
+   Write-Host " &"   -ForegroundColor White -NoNewline 
+   Write-Host " ""npm audit fix""" -ForegroundColor Green
+   npm install --save-dev check-dependencies
+   npm audit fix 
+   return
 }
 else {
-   "Checking dependencies"
+   Write-Host "Checking npm package dependencies in project" -ForegroundColor Yellow
    node node_modules/check-dependencies/bin/cli check-dependencies --install
+   return
 }
